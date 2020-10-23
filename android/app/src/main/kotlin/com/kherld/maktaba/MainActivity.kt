6750dbe5bf581fd0daf98186cd8a7417252f16cc
kherld-hussein <eternety11@gmail.com>
2020-10-24 01:45:51 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/kherld/maktaba/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kherld.maktaba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:maktaba/screens/home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maktaba',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
